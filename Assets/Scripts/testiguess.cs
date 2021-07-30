0246a2123641a860132c07431d72ab2c6a38d63f
CRIMSON_SLAB\Sebastian <lordsebreal@gmail.com>
2021-07-30 16:57:44 +0100

    The rocks... they fall.

--- Assets/Scripts/testiguess.cs
+++ Assets/Scripts/testiguess.cs
@@ -10,9 +10,27 @@ public class testiguess : MonoBehaviour
     {
         Vector3 position = new Vector3(0, 2, 0);
         Instantiate(rock, position, Quaternion.identity);
+        Debug.Log(RandomNumber());
 
+        int rockR = RandomNumber();
+        int rockI = 0;
+        if(rockI == 0)
+        {
+            for (rockI = 0; rockI < rockR; rockI++)
+            {
+                position.y += 1;
+                position.z = RandomNumber();
+                Instantiate(rock, position, Quaternion.identity);
+            }
+        }
+       
     }
-
+    int RandomNumber()
+    {
+        int num = Random.Range(0, 10);
+        return num;
+    }
+    
     // Update is called once per frame
     void Update()
     {
